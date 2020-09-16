5806edfc6f05d111a1411664b974b62180fe475f
Karthik S Kashyap <karthikkashyap98@gmail.com>
2020-09-17 02:10:55 +0530

    User login and signup, storing messages     - Login, logout, signup     - Storing messages in DB     - Render old messages with Jinja2

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Messages(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat_room = models.CharField(max_length=200)
+    message = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.username} - {self.chat_room} - {self.timestamp}"
