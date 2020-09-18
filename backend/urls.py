68911da4e0148151a3157807c402fbfa3e33d011
Karthik S Kashyap <karthikkashyap98@gmail.com>
2020-09-18 19:55:39 +0530

    Send home function to route everything to chat app

--- backend/urls.py
+++ backend/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render, HttpResponse, redirect
 from django.conf.urls.static import static
 from django.conf import settings
 
 
+def send_home(request):
+    return redirect("/chat/")
+
+
 urlpatterns = [
+    path("",  send_home),
     path('admin/', admin.site.urls),
     path('chat/', include('api.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
